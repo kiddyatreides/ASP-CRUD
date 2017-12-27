55f6e5e1f61b38b52495140d5e8e3586b5c9bca0
Kiddy <Kiddy>
2017-12-27 20:59:53 +0700

    Create Feature Added!

---
+++ LatihanASP/Create.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true"
+    CodeBehind="Create.aspx.cs" Inherits="LatihanASP.Create" %>
+
+<asp:Content ID="BodyContent" runat="server" ContentPlaceHolderID="MainContent">
+    <!-- Remove This Before You Start -->
+    <h2>Tambah Barang PT. Oke Oce</h2>
+    <hr />
+            <form id="Form1" method="post" runat="server">
+                <div class="form-group">
+                    <label for="nama">Nama:</label>
+                    <asp:TextBox ID="nama" CssClass="form-control" runat="server"></asp:TextBox>
+                </div>
+                <div class="form-group">
+                    <label for="email">Deskripsi:</label>
+                    <asp:TextBox ID="deskripsi" CssClass="form-control" runat="server" TextMode="MultiLine"></asp:TextBox>
+                </div>
+                <div class="form-group">
+                    <label for="nohp">Harga:</label>
+                    <asp:TextBox ID="harga" CssClass="form-control" runat="server"></asp:TextBox>
+                </div>
+                <div class="form-group">
+                    <asp:Button ID="btn_save" CssClass="btn btn-md btn-primary" OnClick="save" runat="server" Text="Save" />
+                    <button type="reset" class="btn btn-md btn-danger">Cancel</button>
+                </div>
+</form>
+</asp:Content>
---
+++ LatihanASP/Create.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace LatihanASP
+{
+    public partial class Create : System.Web.UI.Page
+    {
+
+        string connStr = ConfigurationManager.ConnectionStrings["myCon"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void save(Object sender, EventArgs e)
+        {
+            try
+            {
+                string sql = "insert into barang (nama,deskripsi,harga) values (@nama, @deskripsi, @harga)";
+                SqlConnection conn = new SqlConnection(connStr);
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                conn.Open();
+
+                cmd.Parameters.Add(new SqlParameter("@nama", SqlDbType.VarChar, 250));
+                cmd.Parameters.Add(new SqlParameter("@deskripsi", SqlDbType.Text));
+                cmd.Parameters.Add(new SqlParameter("@harga", SqlDbType.Money));
+
+                cmd.Parameters["@nama"].Value = nama.Text.ToString();
+                cmd.Parameters["@deskripsi"].Value = deskripsi.Text.ToString();
+                cmd.Parameters["@harga"].Value = harga.Text.ToString();
+
+                cmd.ExecuteNonQuery();
+                conn.Close();
+
+                Response.Write("<script>alert('Berhasil');</script>");
+                Response.Write("<script>window.location='View.aspx';</script>");
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('"+ ex.Message +"');</script>");
+            }
+        }
+    }
+}
